9ce5ee7cb87c17b7d29d60d578c74b1141771345
constXife <constxife@yandex.ru>
2017-10-09 09:54:15 +0700

    Release 0.1.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_agreement do
   name 'Redmine Agreement plugin'
   url 'https://github.com/centosadmin/redmine_agreement'
   description 'This is a plugin for Redmine to add agreement'
-  version '0.1.0'
+  version '0.1.1'
   author 'Southbridge'
   author_url 'https://github.com/centosadmin'
 
