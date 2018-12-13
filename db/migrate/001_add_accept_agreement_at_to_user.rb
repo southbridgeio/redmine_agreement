aaad785db4e09626e716038cfb0ff2461e2ee753
Vladislav Yashin <v.yashin.work@gmail.com>
2018-12-13 13:34:20 +0400

    Adapt for Redmine 4

--- db/migrate/001_add_accept_agreement_at_to_user.rb
+++ db/migrate/001_add_accept_agreement_at_to_user.rb
@@ -1,4 +1,4 @@
-class AddAcceptAgreementAtToUser < ActiveRecord::Migration
+class AddAcceptAgreementAtToUser < Rails.version < '5.0' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     add_column :users, :accept_agreement_at, :datetime
   end
