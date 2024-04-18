92e83fbdf7ae387da0aeda0a6e67b95ae78090e2
Anastasiya Novikova <annovikova18@yandex.ru>
2024-04-18 16:53:10 +0400

    add compatibility with Redmine 5.1

--- lib/redmine_agreement/patches/application_controller_patch.rb
+++ lib/redmine_agreement/patches/application_controller_patch.rb
@@ -1,47 +1,50 @@
 module RedmineAgreement
-  module ApplicationControllerPatch
-    def self.included(base) # :nodoc:
-      base.class_eval do
-        unloadable
+  module Patches
+    module ApplicationControllerPatch
+      def self.included(base)
+        # :nodoc:
+        base.class_eval do
+          unloadable
 
-        before_action :check_agreement
+          before_action :check_agreement
 
-        private
+          private
 
-        def check_agreement
-          if need_accept_agreement? && !allowed_path?
-            redirect_to plugin_settings['agreement_page']
+          def check_agreement
+            if need_accept_agreement? && !allowed_path?
+              redirect_to plugin_settings['agreement_page']
+            end
           end
-        end
 
-        def need_accept_agreement?
-          plugin_settings['enable_agreement'] &&
+          def need_accept_agreement?
+            plugin_settings['enable_agreement'] &&
               plugin_settings['agreement_page'].present? &&
               plugin_settings['reject_agreement_page'].present? &&
               !user.anonymous? &&
               !user.admin? &&
               !user.must_change_password? &&
               !user.accept_agreement?
-        end
+          end
 
-        def allowed_path?
-          [
+          def allowed_path?
+            [
               accept_agreements_path,
               reject_agreements_path,
               plugin_settings['agreement_page'],
               plugin_settings['reject_agreement_page']
-          ].include?(request.original_fullpath)
-        end
+            ].include?(request.original_fullpath)
+          end
 
-        def user
-          User.current
-        end
+          def user
+            User.current
+          end
 
-        def plugin_settings
-          Setting.plugin_redmine_agreement
+          def plugin_settings
+            Setting.plugin_redmine_agreement
+          end
         end
       end
     end
   end
 end
-ApplicationController.send(:include, RedmineAgreement::ApplicationControllerPatch)
+ApplicationController.send(:include, RedmineAgreement::Patches::ApplicationControllerPatch)
--- lib/redmine_agreement/patches/user_patch.rb
+++ lib/redmine_agreement/patches/user_patch.rb
@@ -1,16 +1,19 @@
 module RedmineAgreement
-  module UserPatch
-    def self.included(base) # :nodoc:
-      base.class_eval do
-        unloadable
+  module Patches
+    module UserPatch
+      def self.included(base)
+        # :nodoc:
+        base.class_eval do
+          unloadable
 
-        def accept_agreement?
-          self.accept_agreement_at
+          def accept_agreement?
+            self.accept_agreement_at
+          end
+
         end
-
       end
-    end
 
+    end
   end
 end
-User.send(:include, RedmineAgreement::UserPatch)
+User.send(:include, RedmineAgreement::Patches::UserPatch)
